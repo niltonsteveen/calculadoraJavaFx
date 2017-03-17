ef1294efea388484f805b14e1f69601983d900bb
nilto <nilton.velez@udea.edu.co>
2017-03-17 18:51:07 -0500

    Se realizan las operaciones básicas con la mayoria de excepciones, se esta haciendo la funcionalidad siguiendo el comportamiento de la calculadora de windows (estandar)

--- calculadora/src/FXMLEstandarCalculatorController.java
+++ calculadora/src/FXMLEstandarCalculatorController.java
@@ -20,6 +20,7 @@ import javafx.scene.control.TextField;
  */
 public class FXMLEstandarCalculatorController implements Initializable {
     Utils utils;
+    StringBuffer txtOp1=new StringBuffer();
     
     @FXML    private JFXButton num4;
 
@@ -46,7 +47,23 @@ public class FXMLEstandarCalculatorController implements Initializable {
     @FXML    private JFXButton num0;
 
     @FXML    private TextField textoOperacion;
+    
+    @FXML    private JFXButton borrarTodo;
 
+    @FXML    private JFXButton accionSqrt;
+    
+    @FXML    private JFXButton operadorMen;
+    
+    @FXML    private JFXButton operadorMult;
+    
+    @FXML    private JFXButton operadorDiv;
+
+    @FXML    private JFXButton accionModulo;
+    
+    @FXML    private JFXButton accionDecimal;
+    
+    @FXML    private TextField textoOperacion1;
+    
     @FXML
     void accionRealizada(ActionEvent event) {
         Button btnClickeado=(Button)event.getSource();
@@ -54,47 +71,181 @@ public class FXMLEstandarCalculatorController implements Initializable {
         switch(btn){
             case "num1":
                 utils.addString(num1.getText());
-                textoOperacion.setText(utils.getEntrada().toString());
+                txtOp1.append(num1.getText());
+                textoOperacion1.setText(txtOp1.toString());
                 break;
             case "num2":
                 utils.addString(num2.getText());
-                textoOperacion.setText(utils.getEntrada().toString());
+                txtOp1.append(num2.getText());
+                textoOperacion1.setText(txtOp1.toString());
                 break;
             case "num3":
                 utils.addString(num3.getText());
-                textoOperacion.setText(utils.getEntrada().toString());
+                txtOp1.append(num3.getText());
+                textoOperacion1.setText(txtOp1.toString());
                 break;
             case "num4":
                 utils.addString(num4.getText());
-                textoOperacion.setText(utils.getEntrada().toString());
+                txtOp1.append(num4.getText());
+                textoOperacion1.setText(txtOp1.toString());
                 break;
             case "num5":
                 utils.addString(num5.getText());
-                textoOperacion.setText(utils.getEntrada().toString());
+                txtOp1.append(num5.getText());
+                textoOperacion1.setText(txtOp1.toString());
                 break;
             case "num6":
                 utils.addString(num6.getText());
-                textoOperacion.setText(utils.getEntrada().toString());
+                txtOp1.append(num6.getText());
+                textoOperacion1.setText(txtOp1.toString());
                 break;
             case "num7":
                 utils.addString(num7.getText());
-                textoOperacion.setText(utils.getEntrada().toString());
+                txtOp1.append(num7.getText());
+                textoOperacion1.setText(txtOp1.toString());
                 break;
             case "num8":
                 utils.addString(num8.getText());
-                textoOperacion.setText(utils.getEntrada().toString());
+                txtOp1.append(num8.getText());
+                textoOperacion1.setText(txtOp1.toString());
                 break;
             case "num9":
                 utils.addString(num9.getText());
-                textoOperacion.setText(utils.getEntrada().toString());
+                txtOp1.append(num9.getText());
+                textoOperacion1.setText(txtOp1.toString());
                 break;
             case "num0":
                 utils.addString(num0.getText());
-                textoOperacion.setText(utils.getEntrada().toString());
+                txtOp1.append(num0.getText());
+                textoOperacion1.setText(txtOp1.toString());
                 break;
             case "operadorSum":
-                utils.addString(operadorSum.getText());
-                textoOperacion.setText(utils.getEntrada().toString());
+                if(!Character.isDigit(utils.getEntrada().charAt(utils.getEntrada().length()-1))){
+                    utils.getEntrada().replace(utils.getEntrada().length()-1, utils.getEntrada().length(), "+");
+                    textoOperacion.setText(utils.getEntrada().toString());
+                    utils.operar(txtOp1.toString(),operadorSum.getText());
+                    if(utils.isIsZero()){
+                        textoOperacion.setText("No se puede dividir por Cero");
+                        textoOperacion1.setText("");
+                        utils.setEntrada(null);
+                    }
+                }else if(utils.numOperandos()>0){
+                    int subs=(utils.getEntrada().length())-(txtOp1.length()+1);
+                    utils.operar(txtOp1.toString(), (utils.getEntrada().substring(subs,subs+1)));
+                    utils.addString(operadorSum.getText());
+                    textoOperacion.setText(utils.getEntrada().toString());
+                    textoOperacion1.setText(String.valueOf(utils.getResultado()));
+                    txtOp1.delete(0, txtOp1.length());
+                    if(utils.isIsZero()){
+                        textoOperacion.setText("No se puede dividir por Cero");
+                        textoOperacion1.setText("");
+                        utils.setEntrada(null);
+                    }
+                }else{
+                    utils.addString(operadorSum.getText());
+                    textoOperacion.setText(utils.getEntrada().toString());
+                    utils.operar(txtOp1.toString(),operadorSum.getText());
+                    textoOperacion1.setText(String.valueOf(utils.getResultado()));
+                    txtOp1.delete(0, txtOp1.length());
+                    if(utils.isIsZero()){
+                        textoOperacion.setText("No se puede dividir por Cero");
+                        textoOperacion1.setText("");
+                        utils.setEntrada(null);
+                    }
+                }
+                break;
+            case "operadorMen":
+                if(!Character.isDigit(utils.getEntrada().charAt(utils.getEntrada().length()-1))){
+                    utils.getEntrada().replace(utils.getEntrada().length()-1, utils.getEntrada().length(), "-");
+                    textoOperacion.setText(utils.getEntrada().toString());
+                    utils.operar(txtOp1.toString(),operadorMen.getText());
+                    if(utils.isIsZero()){
+                        textoOperacion.setText("No se puede dividir por Cero");
+                        textoOperacion1.setText("");
+                        utils.setEntrada(null);
+                    }
+                }else if(utils.numOperandos()>0){
+                    int subs=(utils.getEntrada().length())-(txtOp1.length()+1);
+                    utils.operar(txtOp1.toString(), (utils.getEntrada().substring(subs,subs+1)));
+                    utils.addString(operadorMen.getText());
+                    textoOperacion.setText(utils.getEntrada().toString());
+                    textoOperacion1.setText(String.valueOf(utils.getResultado()));
+                    txtOp1.delete(0, txtOp1.length());
+                    if(utils.isIsZero()){
+                        textoOperacion.setText("No se puede dividir por Cero");
+                        textoOperacion1.setText("");
+                        utils.setEntrada(null);
+                    }
+                }else{
+                    utils.addString(operadorMen.getText());
+                    textoOperacion.setText(utils.getEntrada().toString());
+                    utils.operar(txtOp1.toString(),operadorMen.getText());
+                    textoOperacion1.setText(String.valueOf(utils.getResultado()));
+                    txtOp1.delete(0, txtOp1.length());
+                    if(utils.isIsZero()){
+                        textoOperacion.setText("No se puede dividir por Cero");
+                        textoOperacion1.setText("");
+                        utils.setEntrada(null);
+                    }
+                }     
+                break;
+            case "operadorMult":
+                if(!Character.isDigit(utils.getEntrada().charAt(utils.getEntrada().length()-1))){
+                    utils.getEntrada().replace(utils.getEntrada().length()-1, utils.getEntrada().length(), "*");
+                    textoOperacion.setText(utils.getEntrada().toString());
+                    utils.operar(txtOp1.toString(),operadorMult.getText());
+                }else if(utils.numOperandos()>0){
+                    int subs=(utils.getEntrada().length())-(txtOp1.length()+1);
+                    utils.operar(txtOp1.toString(), (utils.getEntrada().substring(subs,subs+1)));
+                    utils.addString(operadorMult.getText());
+                    textoOperacion.setText(utils.getEntrada().toString());
+                    textoOperacion1.setText(String.valueOf(utils.getResultado()));
+                    txtOp1.delete(0, txtOp1.length());
+                }else{
+                    utils.addString(operadorMult.getText());
+                    textoOperacion.setText(utils.getEntrada().toString());
+                    utils.operar(txtOp1.toString(),operadorMult.getText());
+                    textoOperacion1.setText(String.valueOf(utils.getResultado()));
+                    txtOp1.delete(0, txtOp1.length());
+                }     
+                break;
+            case "operadorDiv":
+                if(!Character.isDigit(utils.getEntrada().charAt(utils.getEntrada().length()-1))){
+                    utils.getEntrada().replace(utils.getEntrada().length()-1, utils.getEntrada().length(), "/");
+                    textoOperacion.setText(utils.getEntrada().toString());
+                    utils.operar(txtOp1.toString(),operadorDiv.getText());
+                    if(utils.isIsZero()){
+                        textoOperacion.setText("No se puede dividir por Cero");
+                        textoOperacion1.setText("");
+                        utils.setEntrada(null);
+                    }
+                }else if(utils.numOperandos()>0){
+                    int subs=(utils.getEntrada().length())-(txtOp1.length()+1);
+                    utils.operar(txtOp1.toString(), (utils.getEntrada().substring(subs,subs+1)));
+                    if(utils.isIsZero()){
+                        textoOperacion.setText("No se puede dividir por Cero");
+                        textoOperacion1.setText("");
+                        utils.setEntrada(null);
+                    }else{
+                        utils.addString(operadorDiv.getText());
+                        textoOperacion.setText(utils.getEntrada().toString());
+                        textoOperacion1.setText(String.valueOf(utils.getResultado()));
+                        txtOp1.delete(0, txtOp1.length());
+                        
+                    }
+                }else{
+                    utils.addString(operadorDiv.getText());
+                    textoOperacion.setText(utils.getEntrada().toString());
+                    utils.operar(txtOp1.toString(),operadorDiv.getText());
+                    if(utils.isIsZero()){
+                        textoOperacion.setText("No se puede dividir por Cero");
+                        textoOperacion1.setText("");
+                        utils.setEntrada(null);
+                    }else{
+                        textoOperacion1.setText(String.valueOf(utils.getResultado()));
+                        txtOp1.delete(0, txtOp1.length());
+                    }
+                }     
                 break;
             case "operadorIgual":
                 String texto=utils.getEntrada().toString();
--- calculadora/src/Utils.java
+++ calculadora/src/Utils.java
@@ -10,6 +10,26 @@
  */
 public class Utils {
     private StringBuffer entrada;
+    private double resultado=0;
+    private boolean isZero=false;
+    
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
+    public boolean isIsZero() {
+        return isZero;
+    }
+
+    public void setIsZero(boolean isZero) {
+        this.isZero = isZero;
+    }
+    
+    
     
     public Utils() {
         entrada=new StringBuffer();
@@ -22,15 +42,48 @@ public class Utils {
     public void setEntrada(StringBuffer entrada) {
         this.entrada = entrada;
     }
-    
-    
-    
-    public double sumar(double num1, double num2){
-        return num1+num2;
-    }
 
     public void addString(String text) {
         entrada.append(text);
+    }
+
+    public double sumar(double num1,double num2){
+        return num1+num2;
+    }
+    
+    public int numOperandos(){
+        int contador=0;
+        for(int i=0;i<entrada.length();i++){
+            if(entrada.charAt(i)=='+'||entrada.charAt(i)=='-'||entrada.charAt(i)=='*'
+                    ||entrada.charAt(i)=='/'){
+                contador++;
+            }
+        }
+        return contador;
+    }
+    
+    public double operar(String num, String operador) {
+        if(!"".equals(num)){
+            if(operador.equals("+")){
+                resultado=resultado+Double.parseDouble(num);
+            }else if(operador.equals("-")){
+                if(numOperandos()==1){
+                    resultado=Double.parseDouble(num)-resultado;
+                }else{
+                    resultado=resultado-Double.parseDouble(num);
+                }
+            }else if(operador.equals("/")){
+                if(Double.parseDouble(num)==0){
+                    isZero=true;
+                }else if(numOperandos()==1&&entrada.charAt(entrada.length()-1)=='/'){
+                    resultado=Double.parseDouble(num);
+                }else{
+                    resultado=resultado/Double.parseDouble(num);
+                }
+            }
+        }
+        
+        return resultado;
     }
     
 }
